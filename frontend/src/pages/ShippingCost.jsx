a4472cc8cacfc7be51311f39ea9a21b5b9380154
Shreeshail Chavan <shreeshailchavan309@gmail.com>
2024-11-24 05:39:44 +0530

    Update ShippingCost.jsx

--- frontend/src/pages/ShippingCost.jsx
+++ frontend/src/pages/ShippingCost.jsx
@@ -55,7 +55,7 @@ const ShippingCost = () => {
     };
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/get_shipping/', {
+      const response = await fetch('http://13.16.115.78:8000/api/get_shipping/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
